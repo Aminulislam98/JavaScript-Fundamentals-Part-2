4e23afbc7b9c8c82f3a0c134c9d186b6820e02b4
Aminul islam <aminurrohman98@gmail.com>
2026-06-15 11:46:08 +0100

    Clarifies strict-mode comments and reserved words
    Clarifies inline documentation to better explain why strict mode helps catch undeclared variables, reducing confusion for readers and future debugging.
    
    Adds brief notes that "interface" and "private" are reserved words in JavaScript.

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 "use strict";
-// learned about strict mode that is help to find bug that can javascript engine silently ignore like normally I can write isDriverLicense = true; although I did not add any variable in this case javascript engine will not give eny error , but if i use strick mode javaScript engine will give error that isDriverLicense is not defined that is actually right:
+// learned about strict mode that is helping  to find bug that can javascript engine silently ignore ,like normally I can write isDriverLicense = true; although I did not add any variable in this case javascript engine will not give eny error , but if i use strick mode javaScript engine will give error that isDriverLicense is not defined that is actually right:
+// interface is reserved word in js
+// private is reserved word in js
