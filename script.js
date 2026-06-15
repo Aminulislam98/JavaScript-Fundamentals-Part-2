e64dc6dab0122a18870bfb0759dcf74e97bcbce3
Aminul islam <aminurrohman98@gmail.com>
2026-06-15 10:53:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,2 @@
+"use strict";
+// learned about strict mode that is help to find bug that can javascript engine silently ignore like normally I can write isDriverLicense = true; although I did not add any variable in this case javascript engine will not give eny error , but if i use strick mode javaScript engine will give error that isDriverLicense is not defined that is actually right:
