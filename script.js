7ee16fbb0f537c8a2d49f2b4b6ecc69e5938aa1b
Aminul islam <aminurrohman98@gmail.com>
2026-06-19 20:45:45 +0100

    Practiced about while loop

--- script.js
+++ script.js
@@ -231,3 +231,11 @@ for (let exercise = 1; exercise < 4; exercise++) {
     // console.log(`Lifting weights repetition ${rep} 🏋️‍♂️`);
   }
 }
+
+// The while Loop;
+let dice = Math.trunc(Math.random() * 6) + 1;
+while (dice !== 6) {
+  // console.log(`You rolled ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to end....");
+}
